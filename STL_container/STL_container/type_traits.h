40ee26d21818b71e6414a78ea28d7711c532cec6
mamengchen <624718596@qq.com>
2019-10-31 20:05:44 +0800

    stl

--- STL_container/STL_container/type_traits.h
+++ STL_container/STL_container/type_traits.h
@@ -1,23 +1,45 @@
-#pragma once
+ï»¿#ifndef MYTINYSTL_TYPE_TRAITS_H_
+#define MYTINYSTL_TYPE_TRAITS_H_
 
-// ÓÃÓÚÌáÈ¡ÀàĞÍĞÅÏ¢
+// è¿™ä¸ªå¤´æ–‡ä»¶ç”¨äºæå–ç±»å‹ä¿¡æ¯
+
+// use standard header for type_traits
 #include <type_traits>
 
-namespace mystl {
-	template <class T, T v>
-	struct m_integral_constant {
-		static constexpr T value = v;
-	};
+namespace mystl
+{
 
-	template <bool b>
-	using m_bool_constant = m_integral_constant<bool, b>;
-	typedef m_bool_constant<true> m_true_type;
-	typedef m_bool_constant<false> m_false_type;
+// helper struct
 
-	template<class T1,class T2>
-	struct pair;
-	template<class T>
-	struct is_pair : mystl::m_false_type {};
-	template<class T1,class T2>
-	struct is_pair<mystl::pair<T1, T2>> :mystl::m_true_type {};
-}
+template <class T, T v>
+struct m_integral_constant
+{
+  static constexpr T value = v;
+};
+
+template <bool b>
+using m_bool_constant = m_integral_constant<bool, b>;
+
+typedef m_bool_constant<true>  m_true_type;
+typedef m_bool_constant<false> m_false_type;
+
+/*****************************************************************************************/
+// type traits
+
+// is_pair
+
+// --- forward declaration begin
+template <class T1, class T2>
+struct pair;
+// --- forward declaration end
+
+template <class T>
+struct is_pair : mystl::m_false_type {};
+
+template <class T1, class T2>
+struct is_pair<mystl::pair<T1, T2>> : mystl::m_true_type {};
+
+} // namespace mystl
+
+#endif // !MYTINYSTL_TYPE_TRAITS_H_
+
