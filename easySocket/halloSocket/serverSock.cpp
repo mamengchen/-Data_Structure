337784c84e22da3349fd2a97a37061ebf7ccf8e8
mamengchen <624718596@qq.com>
2020-02-18 06:14:00 +0800

    简单是实现命令的执行

---
+++ easySocket/halloSocket/serverSock.cpp
@@ -0,0 +1,75 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <stdio.h>
+
+//#pragma comment(lib,"ws2_32.lib")
+
+
+
+int main() {
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+	
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;
+	_sin.sin_port = htons(4567);
+	_sin.sin_addr.S_un.S_addr = INADDR_ANY;
+	//INADDR_ANY 可以用任何ip地址区访问
+	if (SOCKET_ERROR == bind(sock, (sockaddr *)&_sin, sizeof(_sin))) {
+		printf("ERROR,绑定失败..\n");
+	}
+	else {
+		printf("绑定成功...\n");
+	}
+
+	if (SOCKET_ERROR == listen(sock, 5)) {
+		printf("错误，监听网络端口失败...\n");
+	}
+	else {
+		printf("监听网络端口成功...\n");
+	}
+
+	sockaddr_in clientAddr = {};
+	int clientLen = sizeof(clientAddr);
+	SOCKET cliSock = INVALID_SOCKET;
+	
+	cliSock = accept(sock, (sockaddr*)&clientAddr, &clientLen);
+	if (INVALID_SOCKET == cliSock) {
+		printf("错误，接受到无效客户端SOCKET...\n");
+	}
+	printf("新客户端加入: IP = %s\n", inet_ntoa(clientAddr.sin_addr));
+	
+	
+	while (true) {
+		char recvbuf[1024] = {0};
+		int nLen = recv(cliSock, recvbuf, 1024, 0);
+		if (nLen <= 0) {
+			printf("客户端退出...\n");
+			break;
+		}
+
+		if (0 == strcmp(recvbuf, "getName")) {
+			char sentbuf[] = "LiHua";
+			send(cliSock, sentbuf, strlen(sentbuf) + 1, 0);
+		}
+		else if (0 == strcmp(recvbuf, "getYear")) {
+			char sentbuf[] = "20years";
+			send(cliSock, sentbuf, strlen(sentbuf) + 1, 0);
+		}
+		else {
+			char sentbuf[] = "????...";
+			send(cliSock, sentbuf, strlen(sentbuf) + 1, 0);
+		}
+	}
+	closesocket(sock);
+
+	WSACleanup();
+	getchar();
+	return 0;
+}
---
+++ easySocket/halloclient/client.cpp
@@ -0,0 +1,62 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <stdio.h>
+
+
+
+
+
+int main() {
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == sock) {
+		printf("建立socket失败...\n");
+	}
+	else {
+		printf("建立socket成功...\n");
+	}
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;
+	_sin.sin_port = htons(4567);
+	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	int ret = connect(sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
+	if (SOCKET_ERROR == ret) {
+		printf("连接失败..\n");
+	}
+	else {
+		printf("连接成功...\n");
+	}
+
+	
+	while (true) {
+		char sendbuf[1024] = {};
+		char recvbuf[1024] = {};
+		scanf("%s", sendbuf);
+		if (0 == strcmp(sendbuf, "exit")) {
+			printf("收到exit命令退出\n");
+			break;
+		}
+		else {
+			send(sock, sendbuf, strlen(sendbuf) + 1, 0);
+		}
+		int nlen = recv(sock, recvbuf, 1024, 0);
+		if (nlen > 0) {
+			printf("接受到数据: %s\n", recvbuf);
+		}
+	}
+	
+	
+	
+	closesocket(sock);
+
+	WSACleanup();
+	getchar();
+	return 0;
+}
